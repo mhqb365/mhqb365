c2c547eb04ac9825b9a188b446692a70217c63d3
mhqb365 <mhqb365@gmail.com>
2023-06-04 19:16:26 +0700

    finish-battery-windows

--- routes/index.js
+++ routes/index.js
@@ -21,19 +21,31 @@ From mhqb365.com with ❤️</p>`);
 
 batteryRouter.get("/win", async (req, res, next) => {
   const script = `powercfg /batteryreport /xml /output batteryreport.xml
-    $battery = [xml](Get-Content batteryreport.xml)
+
+  $battery = [xml](Get-Content batteryreport.xml)
   
-    $health = [pscustomobject]@{
-      DesignCapacity = $battery.BatteryReport.Batteries.Battery.DesignCapacity;
-      FullChargeCapacity = $battery.BatteryReport.Batteries.Battery.FullChargeCapacity;
-      Max =  $battery.BatteryReport.Batteries.Battery.FullChargeCapacity / $battery.BatteryReport.Batteries.Battery.DesignCapacity * 100;
-      Fair = 100 - $battery.BatteryReport.Batteries.Battery.FullChargeCapacity / $battery.BatteryReport.Batteries.Battery.DesignCapacity * 100
-    }
+  $health = [pscustomobject]@{
+    Time = $battery.BatteryReport.ReportInformation.ScanTime;
+    Brands = $battery.BatteryReport.Batteries.Battery.Id;
+    Manufacturer = $battery.BatteryReport.Batteries.Battery.Manufacturer;
+    ManufactureDate = $battery.BatteryReport.Batteries.Battery.ManufactureDate;
+    SerialNumber = $battery.BatteryReport.Batteries.Battery.SerialNumber;
+    DesignCapacity = $battery.BatteryReport.Batteries.Battery.DesignCapacity;
+    FullChargeCapacity = $battery.BatteryReport.Batteries.Battery.FullChargeCapacity;
+    MaxCapacity = $battery.BatteryReport.Batteries.Battery.FullChargeCapacity / $battery.BatteryReport.Batteries.Battery.DesignCapacity * 100
+    FailureCapacity = 100 - ($battery.BatteryReport.Batteries.Battery.FullChargeCapacity / $battery.BatteryReport.Batteries.Battery.DesignCapacity * 100);
+    CycleCount = $battery.BatteryReport.Batteries.Battery.CycleCount;
+  }
   
-    Write-Host "Design Capacity: $($health.DesignCapacity) mWh"
-    Write-Host "Full Charge Capacity: $($health.FullChargeCapacity) mWh"
-    Write-Host "Dung luong toi da: $($health.Max)%"
-    Write-Host "Do chai pin: $($health.Fair)%"`;
+  Write-Host "Thoi gian kiem tra: $($health.Time)"
+  Write-Host "Nha san xuat: $($health.Brands) - $($health.Manufacturer)"
+  Write-Host "Ngay san xuat: $($health.ManufactureDate)"
+  Write-Host "So serial: $($health.SerialNumber)"
+  Write-Host "Dung luong thiet ke ban dau: $($health.DesignCapacity) mWh"
+  Write-Host "Dung luong khi duoc sac day: $($health.FullChargeCapacity) mWh"
+  Write-Host "% dung luong toi da: $($health.MaxCapacity)%"
+  Write-Host "% do chai pin: $($health.FailureCapacity)%"
+  Write-Host "So lan sac: $($health.CycleCount)"`;
   const stringBuf = Buffer.from(script);
   res.send(stringBuf);
 });
