d3010cb8b4bbdcecbb024b7630886a89a1ba83c9
mhqb365 <mhqb365@gmail.com>
2023-06-07 15:23:17 +0700

    :rocket:

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
-const activeRouter = require("./routes/active");
+const activeRouter = require("./routes/kms");
 const app = express();
 const port = 8000;
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.static("public"));
-app.use("/active", activeRouter);
+app.use("/kms", activeRouter);
 
 app.listen(port, () => {
   console.log(`mhqb365.com run at ${port}`);
